036978899c96cd461ec5a602472d21b66b7ddc26
sponge254 <g.stuk@outlook.com>
2025-12-21 15:57:47 -0500

    added back access to my keys until i can find a better way (i dont think there is)

--- compose/docker-compose-runner.bat
+++ compose/docker-compose-runner.bat
@@ -5,14 +5,4 @@ for %%f in (*.tar) do (
     docker load -i "%%f"
 )
 
-if not exist ".\secrets" (
-    mkdir ".\secrets"
-)
-
-if not exist ".\secrets\key.txt" (
-    set /p API_KEY=Enter TBA key: 
-    set "API_KEY=!API_KEY: =!"
-    echo !API_KEY! > ".\secrets\key.txt"
-)
-
 docker compose --profile prod up
--- compose/docker-compose-runner.sh
+++ compose/docker-compose-runner.sh
@@ -3,12 +3,4 @@ for tar in ./*.tar; do
     docker load -i "$tar"
 done
 
-mkdir -p ./secrets
-
-if [ ! -f "./secrets/key.txt" ]
-    read -p "Enter TBA key: " API_KEY
-    API_KEY=$(echo "$API_KEY" | xargs) # trim
-    echo "$API_KEY" > ./secrets/key.txt
-fi
-
 docker-compose up -d

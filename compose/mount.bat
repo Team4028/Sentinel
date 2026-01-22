976f5621d7e5acc99e83cd15cd46947bfa19e19c
sponge254 <g.stuk@outlook.com>
2026-01-21 19:38:00 -0500

    windows sudo doesn't work for non-admins

--- compose/mount.bat
+++ compose/mount.bat
@@ -8,11 +8,11 @@ if %errorlevel% NEQ 0 (
 )
 
 for /f "tokens=1" %%a in ('usbipd list ^| findstr /C:"USB to UART Bridge"') do set USBID=%%a
-sudo -V >nul 2>&1
+gsudo -V >nul 2>&1
 if %errorlevel% NEQ 0 (
     winget install gerardog.gsudo
 )
-sudo usbipd bind -b %USBID%
+gsudo usbipd bind -b %USBID%
 echo Please continue after the suspicious looking admin cmd window closes
 pause
 usbipd attach -w -b %USBID%

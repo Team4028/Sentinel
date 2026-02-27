125c38ed10fd8ad2d3cc1ec9a8814f401dd510a5
sponge254 <g.stuk@outlook.com>
2026-02-26 20:02:07 -0500

    release in correct form

--- build-for-package.bat
+++ build-for-package.bat
@@ -1,3 +1,12 @@
-if not exist "dockerout" mkdir "dockerout"
+mkdir -p dockerout\grafana-dashboard
 docker buildx build -f .\dockerimage\dockerfile -t scouting:latest -o type=docker,dest=dockerout\scouting.tar .
 docker buildx build -f .\grafana-dashboard\dockerfile -t grafana-scouting:latest -o type=docker,dest=dockerout\grafana-scouting.tar .
+cp .\compose.yaml dockerout
+cp .\compose\.env dockerout\.env
+cp .\compose\run.sh dockerout\run.sh
+cp .\compose\mount.bat dockerout\mount.bat
+wsl chmod +x dockerout/run.sh
+cp .\compose\run.bat dockerout\run.bat
+cp .\grafana-dashboard\grafana.ini dockerout\grafana-dashboard\grafana.ini
+7z a scouting-app.zip dockerout
+rmdir /s /q dockerout

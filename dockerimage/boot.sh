cf94e0687c7f85cca3d6ecefb1f6deb41fbfba33
sponge254 <g.stuk@outlook.com>
2026-03-14 20:02:31 -0400

    remove https :(

--- dockerimage/boot.sh
+++ dockerimage/boot.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # run the flask server on port 5000, get app from scouting_app.py
+USE_HTTPS=false
 mkdir -p /app/log/gunicorn
-mkdir -p /app/secrets
 
-if [ ! -f "/app/secrets/sentinel-key.pem" ]; then
-    python src/app.py sign
+
+if [ "$USE_HTTPS" = "true" ]; then
+    mkdir -p /app/secrets
+    if [ ! -f "/app/secrets/sentinel-key.pem" ]; then
+        python src/app.py sign
+    fi
+    exec gunicorn --certfile=/app/secrets/certinel.pem --keyfile=/app/secrets/sentinel-key.pem -b :443 --log-config /app/config/logging.conf scouting_app:app
+else
+    exec gunicorn -b :5000 --log-config /app/config/logging.conf scouting_app:app
 fi
-
-exec gunicorn --certfile=/app/secrets/certinel.pem --keyfile=/app/secrets/sentinel-key.pem -b :443 --log-config /app/config/logging.conf scouting_app:app
-
 # warning: NEEDS TO BE LF AND NOT CRLF
